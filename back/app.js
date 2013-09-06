f233d707f7e7bfc9ee055cb61bb28852f6f3fad8
Rajib Ahmed <l.rajibahmed@gmail.com>
2013-09-06 15:40:22 +0200

    Add template model files.

--- back/app.js
+++ back/app.js
@@ -17,14 +17,14 @@ mongoose.connect( 'mongodb://localhost:27017/adres' );
 
 // all environments
 app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
+//app.set('views', __dirname + '/views');
+//app.set('view engine', 'jade');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
 if ('development' == app.get('env')) {
